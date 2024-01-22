583ae1ca424e437bbb2624206d4b8dec86441bf3
mk-pmb <mk-pmb@users.noreply.github.com>
2024-01-22 09:05:27 +0100

    Add npm scripts "adapter" and "sh".

--- test/put_one_dcmeta_file.sh
+++ test/put_one_dcmeta_file.sh
@@ -4,13 +4,12 @@
 
 function put_one_dcmeta_file () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
-  local DBA_PATH="$(readlink -m -- "$BASH_SOURCE"/../..)"
   if [ -n "$1" ]; then
     exec <"$1" || return $?
   else
     echo "D: reading DC metadata JSON from stdin." >&2
   fi
-  "$DBA_PATH"/adapter.sh update_doi_meta_for_one_dcmetajson_from_command cat
+  npm run adapter update_doi_meta_for_one_dcmetajson_from_command cat
 }
 
 
