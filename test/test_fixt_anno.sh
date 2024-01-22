c13f8a0897cd5612ecf0764c33ed690350c4eefa
mk-pmb <mk-pmb@users.noreply.github.com>
2024-01-22 02:05:08 +0100

    [test] Track which version numbers are first and latest.

--- test/test_fixt_anno.sh
+++ test/test_fixt_anno.sh
@@ -10,11 +10,11 @@ function test_fixt_anno () {
   local QUOT='"' APOS="'"
   local VERS_ID_RGX='^[a-z0-9_.-]+\~[1-9][0-9]*$'
 
-  elp || return $?
+  echo -n 'Lint: '; elp || return $?
 
   local ERR_CNT=0 SXS_CNT=0
   test_fixt_anno__all_vers 'doibot-test-v'
-  test_fixt_anno__all_vers 'esau-rsterr_'
+  # test_fixt_anno__all_vers 'esau-rsterr_'
 
   echo
   [ "$ERR_CNT" == 0 ] || return "$ERR_CNT"$(
@@ -31,14 +31,26 @@ function find_json_prop () {
 
 function test_fixt_anno__all_vers () {
   local FIXT_BFN_NOVERS="test/fixtures/$1"; shift
-  local SRC= ANNO_VER_NUM= CREA1=
-  for SRC in "$FIXT_BFN_NOVERS"[0-9]*.anno.json; do
-    ANNO_VER_NUM="${SRC:${#FIXT_BFN_NOVERS}}"
+  local FIXT_FILE= ANNO_VER_NUM= CREA1=
+  local MIN_VER_NUM= MAX_VER_NUM=
+  for FIXT_FILE in "$FIXT_BFN_NOVERS"[0-9]*.anno.json; do
+    [ -f "$FIXT_FILE" ] || continue
+    ANNO_VER_NUM="${FIXT_FILE:${#FIXT_BFN_NOVERS}}"
     ANNO_VER_NUM="${ANNO_VER_NUM%.anno.json}"
-    [ -n "$CREA1" ] || CREA1="$(find_json_prop created <"$SRC")"
+    [ -n "$MIN_VERS" ] || MIN_VERS="$ANNO_VER_NUM"
+    [ "$MIN_VERS" -lt "$ANNO_VER_NUM" ] || MIN_VERS="$ANNO_VER_NUM"
+    [ -n "$MAX_VERS" ] || MAX_VERS="$ANNO_VER_NUM"
+    [ "$MAX_VERS" -gt "$ANNO_VER_NUM" ] || MAX_VERS="$ANNO_VER_NUM"
+  done
+  ANNO_VER_NUM=0
+  while [ "$ANNO_VER_NUM" -lt "$MAX_VERS" ]; do
+    (( ANNO_VER_NUM += 1 ))
+    FIXT_FILE="$FIXT_BFN_NOVERS$ANNO_VER_NUM.anno.json"
+    [ -f "$FIXT_FILE" ] || continue
+    [ -n "$CREA1" ] || CREA1="$(find_json_prop created <"$FIXT_FILE")"
     [ -n "$CREA1" ] || return 5$(echo "E: no CREA1" >&2)
-    test_fixt_anno__one_ver "$ANNO_VER_NUM" && continue
-    echo W: $FUNCNAME: >&2 "Test failed (rv=$?) for $SRC"
+    test_fixt_anno__one_ver && continue
+    echo W: $FUNCNAME: >&2 "Test failed (rv=$?) for $FIXT_FILE"
     (( ERR_CNT += 1 ))
   done
   if [ -z "$CREA1" ]; then
@@ -50,9 +62,13 @@ function test_fixt_anno__all_vers () {
 
 
 function test_fixt_anno__one_ver () {
-  local ANNO_VER_NUM="$1"
+  [ -n "$CREA1" ] || return 4$(
+    echo E: $FUNCNAME: "Missing creation date of first anno version!" >&2)
   echo
-  echo P: "test $FUNCNAME $FIXT_BFN_NOVERS ~$ANNO_VER_NUM"
+  echo P: "test $FUNCNAME $FIXT_BFN_NOVERS ~$ANNO_VER_NUM$(
+    [ "$ANNO_VER_NUM" == "$MIN_VERS" ] && echo -n ' (first)'
+    [ "$ANNO_VER_NUM" == "$MAX_VERS" ] && echo -n ' (latest)'
+    )"
   [ "${ANNO_VER_NUM:-0}" -ge 1 ] || return 5$(
     echo E: $FUNCNAME: 'Expected a positive number as ANNO_VER_NUM.' >&2)
   local FIXT_VERS_BFN="$FIXT_BFN_NOVERS$ANNO_VER_NUM"
